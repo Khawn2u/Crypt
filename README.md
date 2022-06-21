d5cd45f30893124b6ae35adcf5170f61f33e1632
Khawn2u <76547416+Khawn2u@users.noreply.github.com>
2022-06-21 13:31:15 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -9,8 +9,14 @@ This is a constructor that creates a new Eliptic Curve with paramiters a, b, and
 # crypt.Keccak256
 This is the Keccak256 hash of the input, the input can be text or a Uint8Array.
 
+# crypt.Keccak384
+This is the Keccak384 hash of the input, the input can be text or a Uint8Array.
+
 # crypt.PublicCurve
 This is a constructor taking in as the first paramiter a crypt.ECurve object, second paramiter an array of BigInt with length 2 as a generator point, and as the third paramiter the order of the generator point on the curve difined in the first paramiter.
 
-# crypt.PublicPrameters
-This is a pre-made crypt.PublicCurve Object with already diffined paramiters. This is used as the default curve.
+# crypt.secp256k1
+This is a pre-made crypt.PublicCurve Object with already diffined paramiters, following the secp256k1 curve documentation.
+
+# crypt.secp384r1
+This is a pre-made crypt.PublicCurve Object with already diffined paramiters, following the secp384r1 curve documentation.
--- index.js
+++ index.js
@@ -181,6 +181,12 @@ var Crypt = function() {
 		this.g = genoratorPoint;
 		this.n = order;
 		this.Hash = null;
+		this.OrderBytes = 0;
+		var ordertmp = order;
+		while (ordertmp) {
+			this.OrderBytes++;
+			ordertmp >>= 8n;
+		}
 		this.setSignHashFunction = function(hashFunct) {
 			this.Hash = hashFunct;
 		}
@@ -225,8 +231,7 @@ var Crypt = function() {
 				return this.sign(message,sk);
 			}
 			var signiture = ((s*this.n)+r);
-			var pk = this.getPublicKey(sk);
-			if (this.adressToPublicKey(pk[0])[1] != pk[1]) {
+			if (this.adressToPublicKey(r)[1] != kG[1]) {
 				signiture += this.n*this.n;
 			}
 			return {
@@ -248,13 +253,8 @@ var Crypt = function() {
 			var rinv = self.modInverse(r,this.n);
 			var sR = this.curve.multiply(R,s);
 			var zG = this.curve.multiply(this.g,z);
-			if (!isnegitive) {
-				zG[1] = this.curve.m-zG[1];
-			}
+			zG[1] = this.curve.m-zG[1];
 			var pk = this.curve.multiply(this.curve.add(sR,zG),rinv);
-			if (isnegitive) {
-				pk[1] = this.curve.m-pk[1];
-			}
 			return pk;
 		}
 		this.verify = function(signiture,pk) {
@@ -270,6 +270,23 @@ var Crypt = function() {
 			var X = this.curve.add(p0,p1);
 			var v = X[0]%this.n;
 			return v == r && v != 0n;
+		}
+		this.encryptWithPublicKey = function(Adress) {
+			var rand = self.bufferToBigInt(self.randomBytes(this.OrderBytes))%this.n;
+			var PublicKey = this.adressToPublicKey(Adress);
+			var p = this.curve.multiply(PublicKey,rand);
+			var SharedSecret = this.toAdress(p);
+			var SharedKey = this.Hash(self.bigIntToBuffer(BigInt(SharedSecret)));
+			var Public = this.toAdress(this.curve.multiply(this.g,rand));
+			return {
+				PublicKey:Public,
+				SharedKey:SharedKey
+			};
+		}
+		this.decryptWithPrivateKey = function(PublicVal,PrivateKey) {
+			var SharedSecret = this.toAdress(this.curve.multiply(this.adressToPublicKey(PublicVal),PrivateKey));
+			var SharedKey = this.Hash(self.bigIntToBuffer(BigInt(SharedSecret)));
+			return SharedKey;
 		}
 	}
 	this.SignedMessageToBuffer = function(signiture) {
@@ -538,6 +555,9 @@ var Crypt = function() {
 	this.secp384r1Curve = new self.ECurve(0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000fffffffcn,0xb3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aefn,0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000ffffffffn);
 	this.secp384r1 = new self.PublicCurve(this.secp384r1Curve,[0xaa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7n,0x3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5fn],0xffffffffffffffffffffffffffffffffffffffffffffffffc7634d81f4372ddf581a0db248b0a77aecec196accc52973n);
 	this.secp384r1.setSignHashFunction(this.Keccak384);
+	this.secp521r1Curve = new self.ECurve(0x01fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffcn,0x0051953eb9618e1c9a1f929a21a0b68540eea2da725b99b315f3b8b489918ef109e156193951ec7e937b1652c0bd3bb1bf073573df883d2c34f1ef451fd46b503f00n,0x01ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffn);
+	this.secp521r1 = new self.PublicCurve(this.secp521r1Curve,[0x00c6858e06b70404e9cd9e3ecb662395b4429c648139053fb521f828af606b4d3dbaa14b5e77efe75928fe1dc127a2ffa8de3348b3c1856a429bf97e7e31c2e5bd66n,0x011839296a789a3bc0045c8a5fb42c7d1bd998f54449579b446817afbd17273e662c97ee72995ef42640c550b9013fad0761353c7086a272c24088be94769fd16650n],0x01fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffa51868783bf2f966b7fcc0148f709a5d03bb5c9b8899c47aebb6fb71e91386409n);
+	this.secp521r1.setSignHashFunction(this.Keccak512);
 }
 if (typeof window === 'undefined') {
 	module.exports = Crypt;
